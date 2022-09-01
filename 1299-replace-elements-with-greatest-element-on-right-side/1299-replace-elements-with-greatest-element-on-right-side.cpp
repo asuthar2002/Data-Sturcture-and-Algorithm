29795306a01b0c4ae7173e701d85f600e7507ca6
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-09-01 20:21:23 +0530

    Time: 19 ms (81.99%), Space: 14.5 MB (67.58%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int>ans;
-        int i=arr.size()-2;
-        int element = arr[arr.size()-1];
-        ans.push_back(-1);
+//         vector<int>ans;
+//         int i=arr.size()-2;
+//         int element = arr[arr.size()-1];
+//         ans.push_back(-1);
+//         while(i>=0)
+//         {
+//             if(arr[i]>element)
+//             {
+//                 ans.push_back(element);
+//                 element = arr[i];
+//             }
+//             else ans.push_back(element);
+            
+//             i--;
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+        
+        // solution 2;
+        
+        int n = arr.size()-1;
+        int i = n-1;
+        int element = arr[n];
+        arr[n] = -1;
         while(i>=0)
         {
+            int nxt ;
             if(arr[i]>element)
             {
-                ans.push_back(element);
-                element = arr[i];
+                nxt = arr[i];
+                arr[i] = element;
+                element = nxt;
             }
-            else ans.push_back(element);
-            
+            else {
+                arr[i] = element;
+            }
             i--;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return arr;
+        
     }
 };
