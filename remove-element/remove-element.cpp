3263d8f1624afae9bd86e735f9081ee27ff4597e
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-09-24 20:57:02 +0530

    Time: 5 ms, Memory: 8.8 MB - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+        return nums.size();
+    }
+};
