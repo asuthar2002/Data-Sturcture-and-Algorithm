b7f58a977cc2ccea2b1fc58a1886d1c40982d3fc
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-09-28 15:57:42 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (6.33%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int code(int n,vector<int> &h,vector<int>&dp)
+    // int code(int n,vector<int> &h,vector<int>&dp)
+    // {
+    //     dp[0] = h[0];
+    //     for(int i=1; i<n; i++)
+    //     {
+    //         int take = h[i];
+    //         if(i>1) take = h[i]+dp[i-2];
+    //         int ntake = dp[i-1];
+    //         dp[i] = max(take,ntake);
+    //     }
+    //     return dp[n-1];
+    // }
+    
+    // using memoization ;
+    int code(int n,vector<int> &h,vector<int>&dp,int i)
     {
-        dp[0] = h[0];
-        for(int i=1; i<n; i++)
-        {
-            int take = h[i];
-            if(i>1) take = h[i]+dp[i-2];
-            int ntake = dp[i-1];
-            dp[i] = max(take,ntake);
-        }
-        return dp[n-1];
+        if(i>=n) return 0;
+        
+        if(dp[i]!=-1)return dp[i];
+        int take = code(n,h,dp,i+2)+h[i];
+        int nottake = code(n,h,dp,i+1);
+        dp[i] = max(take,nottake);
+        return dp[i];
+        
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>v(n,-1);
-        return code(n,nums,v);
+        return code(n,nums,v,0);
     }
 };
