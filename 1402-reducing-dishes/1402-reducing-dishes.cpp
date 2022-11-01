b0e31f96a59c62a4a6b734ce84069ceaba19eef0
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-11-01 18:16:25 +0530

    Time: 107 ms (13.43%), Space: 20.9 MB (21.04%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int code(vector<int>&s, int i,int t,vector<vector<int>>&dp)
+    int code(vector<int> &s,int i,int time,vector<vector<int>>&dp)
     {
-        if(i==s.size() ) return 0;
-        if(dp[i][t]!= -1) return dp[i][t];
-        return dp[i][t] =  max(s[i]*(t+1)+code(s,i+1,t+1,dp), code(s,i+1,t,dp));
+        if(i>=s.size()) return 0;
+        if(dp[i][time] != -1) return dp[i][time];
+        return dp[i][time] = max(s[i]*(time+1) + code(s,i+1,time+1,dp), code(s,i+1,time,dp));
     }
     int maxSatisfaction(vector<int>& s) {
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1,-1));
         sort(s.begin(),s.end());
-        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
         return code(s,0,0,dp);
     }
 };
