9716746d931f0e45c8e55586577a7e716dbbc410
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-11-05 09:53:29 +0530

    Time: 741 ms (26.91%), Space: 292.4 MB (5.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -8,8 +8,27 @@ public:
         if(p==-1 || arr[i]>arr[p]) len = 1+code(arr,i+1,i,dp);
         return dp[i][p+1] = max(len,code(arr,i+1,p,dp));
     }
+    
+    int f(vector<int> arr)
+    {
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=i-1; j>=-1; j--)
+            {
+                int take = 0;
+                if(j==-1 || arr[i]>arr[j])
+                    take = 1+dp[i+1][i+1];
+                int ntake = dp[i+1][j+1];
+                dp[i][j+1] = max(take , ntake);
+            }
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return code(nums,0,-1,dp);
+        // vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
+        // return code(nums,0,-1,dp);
+        return f(nums);
     }
 };
