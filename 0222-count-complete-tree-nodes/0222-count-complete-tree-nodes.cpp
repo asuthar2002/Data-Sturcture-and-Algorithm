1813090b3575b39d1797778b4d273ccaf44b23fd
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2022-11-15 08:58:27 +0530

    Time: 77 ms (18.19%), Space: 30.8 MB (93.85%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,30 +11,17 @@
  */
 class Solution {
 public:
-    void code(TreeNode*root,int &cnt)
-    {
-        if(not root) return;
-        code(root->left,cnt);
-        cnt++;
-        code(root->right,cnt);
-    }
-    
-    int code(TreeNode*root)
-    {
+    int code(TreeNode*root){
         if(not root) return 0;
-        int l = 0,r = 0;
-        TreeNode*lr = root; TreeNode*rr = root;
-        while(lr){ l++;lr = lr->left; }
+        int l=0, r=0;
+        TreeNode*lr = root;
+        TreeNode*rr = root;
+        while(lr){ l++; lr = lr->left; }
         while(rr){ r++; rr = rr->right; }
-        if(l==r) return pow(2,l)-1;
+        if(l == r) return pow(2, r)-1;
         return 1+code(root->left)+code(root->right);
     }
     int countNodes(TreeNode* root) {
-        // int cnt = 0;
-        // code(root,cnt);
-        // return cnt;
-        
-        //optimized approach
-        return code(root);
+        return code(root );
     }
 };
