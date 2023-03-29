d05853e3032d5c042ce6c213ca8d609ab4986d52
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2023-03-29 08:54:43 +0530

    Time: 48 ms (36.34%), Space: 20.9 MB (49.34%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int code(vector<int> &s,int i,int time,vector<vector<int>>&dp)
-    {
+    int code(vector<int> &s,int i,int time,vector<vector<int>>&dp){
         if(i>=s.size()) return 0;
         if(dp[i][time] != -1) return dp[i][time];
         return dp[i][time] = max(s[i]*(time+1) + code(s,i+1,time+1,dp), code(s,i+1,time,dp));
