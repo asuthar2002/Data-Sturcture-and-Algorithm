fc57f485e3b86eb57616269a9ada85bc20e1deee
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2023-08-15 10:18:02 +0530

    Time: 4 ms (78.23%), Space: 10.3 MB (48.14%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,27 +11,23 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        ListNode*bignumber = new ListNode(-1);
+        ListNode*smallnumber = new ListNode(-1);
+        ListNode*bignode = bignumber;
+        ListNode*smallnode = smallnumber;
         ListNode*node = head;
-        vector<int> ans;
-        vector<int> arr;
         while(node){
-            ans.push_back(node->val);
-            node = node->next;
-        }
-        for(int i=0;i<ans.size(); i++){
-            if(ans[i]<x) {
-                arr.push_back(ans[i]);
-                ans.erase(ans.begin()+i);
-                i--;
+            if(node->val<x) {
+                smallnode->next = new ListNode(node->val);
+                smallnode = smallnode->next;
             }
+            else {
+                bignode->next = new ListNode(node->val);
+                bignode = bignode->next;
+            }
+             node = node->next;
         }
-    // for(auto it: arr) cout<<it<<" ";
-        for(auto it:ans) arr.push_back(it);
-        node = head;
-        for(int i=0; i<arr.size(); i++){
-            node->val = arr[i];
-            node = node->next;
-        }
-        return head;
+        smallnode->next = bignumber->next;
+        return smallnumber->next;
     }
 };
