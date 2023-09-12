30613b7875822d7f3573097fddb07200c7279f02
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2023-09-12 08:56:24 +0530

    Time: 80 ms (35.52%), Space: 17.3 MB (58.00%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        int arr[26] = {0};
-        for(int i=0; i<s.length(); i++)arr[s[i]-'a']++;
-        sort(arr,arr+26);
-        int del = 0;
-        int prev = 0;
-        for(int i=24; i>=0; i--)
-        {
-            if(arr[i] == 0) break;
-             if(arr[i] >= arr[i+1]){
-                int prev = arr[i];
-                arr[i] = max(0, arr[i+1] -1);
-                del += prev - arr[i]; 
+        unordered_map<char,int>map;
+        for(auto it:s)map[it]++;
+        vector<int> ans;
+        for(auto it: map)ans.push_back(it.second);
+        sort(ans.begin(), ans.end());
+        for(int i=ans.size()-1; i>=0; i--)cout<<ans[i]<<" ";
+        cout<<endl;
+        int cnt = 0;
+        for(int i=ans.size()-2; i>=0; --i){
+            if(ans[i] == 0) break;
+            if(ans[i]>=ans[i+1]){
+                cout<<ans[i]<<' ';
+                int prev = ans[i];
+                ans[i] = max(0, ans[i+1]-1);
+                cnt += prev-ans[i];
             }
-            
         }
-        return del;
+        return cnt;
     }
 };
